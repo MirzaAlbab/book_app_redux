d212b1bb708939150ff871263d96c094c2d121a5
unknown <mirzaalbabfirdaus17@gmail.com>
2022-04-05 16:38:36 +0700

    main

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {persistedStore, store} from './src/store';
 import Root from './src/routes';
+import SplashScreen from 'react-native-splash-screen';
 import {PersistGate} from 'redux-persist/integration/react';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <Provider store={store}>
       <PersistGate persistor={persistedStore}>
