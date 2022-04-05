e3b8f038e411dbebbb88bcab3ff2acbc522b9211
unknown <mirzaalbabfirdaus17@gmail.com>
2022-04-05 16:37:02 +0700

    main

--- android/app/src/main/java/com/bookapp__redux/MainActivity.java
+++ android/app/src/main/java/com/bookapp__redux/MainActivity.java
@@ -1,15 +1,23 @@
 package com.bookapp__redux;
-
+import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // here
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends ReactActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this,true);  // here
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+  protected String getMainComponentName() {
+    return "bookapp__redux";
+  }
+} 
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  @Override
-  protected String getMainComponentName() {
-    return "bookapp__redux";
-  }
-}
+  
+  
