796325479ec8c3299d770cd110317f5f24f5ad31
unknown <mirzaalbabfirdaus17@gmail.com>
2022-04-21 23:08:09 +0700

    fix error build

--- android/app/src/main/java/com/bookapp__redux/MainActivity.java
+++ android/app/src/main/java/com/bookapp__redux/MainActivity.java
@@ -3,7 +3,6 @@ import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen; // here
 
-<<<<<<< HEAD
 public class MainActivity extends ReactActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +15,6 @@ public class MainActivity extends ReactActivity{
   }
 } 
 
-=======
-public class MainActivity extends ReactActivity {
-  @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);  // here
-        super.onCreate(savedInstanceState);
-    }
->>>>>>> featured
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
