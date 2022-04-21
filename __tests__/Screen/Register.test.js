7f9f3f1adb0d09adf26954b565d60b6d0a2cbefc
unknown <mirzaalbabfirdaus17@gmail.com>
2022-04-21 20:40:04 +0700

    update testing again

--- __tests__/Screen/Register.test.js
+++ __tests__/Screen/Register.test.js
@@ -5,6 +5,7 @@ import ContainerTesting from '../../src/helpers/ReduxTesting';
 
 describe('Group Name', () => {
   test('renders correctly', () => {
+    jest.useFakeTimers();
     const tree = create(ContainerTesting(<Register />));
     expect(tree).toMatchSnapshot();
   });
