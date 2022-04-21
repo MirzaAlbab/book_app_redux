766d535f3cc2636adddb8b9372edecf789c42eab
unknown <mirzaalbabfirdaus17@gmail.com>
2022-04-21 22:15:19 +0700

    update yarn, unit testing

--- __tests__/Components/Rupiah.test.js
+++ __tests__/Components/Rupiah.test.js
@@ -4,4 +4,7 @@ describe('Test Rupiah', () => {
   test('should be rupiah', () => {
     expect(Rupiah('1000000')).toBe('Rp. 1.000.000');
   });
+  test('should be rupiah', () => {
+    expect(Rupiah('958343')).toBe('Rp. 958.343');
+  });
 });
