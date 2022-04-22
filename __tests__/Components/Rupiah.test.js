0b6a32af0af944ba4d65fb45faa9e19674f6f005
unknown <mirzaalbabfirdaus17@gmail.com>
2022-04-22 14:53:06 +0700

    fix rupiah testing

--- __tests__/Components/Rupiah.test.js
+++ __tests__/Components/Rupiah.test.js
@@ -4,7 +4,7 @@ describe('Test Rupiah', () => {
   test('should be rupiah', () => {
     expect(Rupiah('1000000')).toBe('Rp. 1.000.000');
   });
-  test('should be rupiah', () => {
+  test('should be rupiah v2', () => {
     expect(Rupiah('958343')).toBe('Rp. 958.343');
   });
 });
